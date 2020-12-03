9fa354c4f15aed47627afcd3408fd91c59213773
Michael Brunner <MichaelBrunn3r@gmail.com>
2020-12-03 10:51:08 +0100

    Populated Table component

--- src/Table.svelte
+++ src/Table.svelte
@@ -1,1 +1,48 @@
-<h1>Table</h1>
+<script>
+	import materialStore from './material-store.js';
+
+	let materials = [];
+
+	materialStore.subscribe(items => {
+		materials = items;
+	})
+
+	const formatter = new Intl.NumberFormat('de-DE',{
+		style: 'currency',
+		currency: 'EUR'
+	});
+
+	$: total = materials.reduce((acc, current) => acc+current.price, 0);
+</script>
+
+<style>
+	table {
+		width: 100%;
+	}
+</style>
+
+<h1>Total: {formatter.format(total)}</h1>
+<table class="primary">
+	<thead>
+		<tr>
+			<th>Material</th>
+			<th>Price</th>
+			<th></th>
+		</tr>
+	</thead>
+	<tbody>
+		{#each materials as material (material.id)}
+			<tr>
+				<td>{material.material}</td>
+				<td>{formatter.format(material.price)}</td>
+				<td>
+					<i class="far fa-trash-alt"></i>
+				</td>
+			</tr>
+		{/each}
+		<tr>
+			<td><b>Total</b></td>
+			<td colspan="2"><b>{formatter.format(total)}</b></td>
+		</tr>
+	</tbody>
+</table>
